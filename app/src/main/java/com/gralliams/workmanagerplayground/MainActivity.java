0ea9423a0fcd8b9ea6c0726e2e23b594abc396d9
Grant Williams <greatgrant116@gmail.com>
2021-01-14 08:18:46 +0100

    Added comments

--- app/src/main/java/com/gralliams/workmanagerplayground/MainActivity.java
+++ app/src/main/java/com/gralliams/workmanagerplayground/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
 import android.os.Bundle;
@@ -20,27 +21,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Create data object to be utilised in Worker Class
+        /* Create data object to be utilised in Worker Class: use this if yourWorRequest and Worker class  requires data */
         Data data = new Data.Builder()
                             .putInt(SampleWorker.NUMBER, 10)
                             .build();
 
-        //Creates constraint for making request
+        /* Create constraint for making request: use this in the WorkRequest using .setConstraints if you need constraints */
         Constraints constraints = new Constraints.Builder()
                                     .setRequiresCharging(true)
                                     .setRequiredNetworkType(NetworkType.CONNECTED)
                                     .build();
 
-        //Create WorkRequest: Can be one time or periodic. We'll be using one time in this case
+        /* Create WorkRequest: Can be one time or periodic. We'll be using one time in this case */
         OneTimeWorkRequest oneTimeDownloadRequest = new OneTimeWorkRequest.Builder(SampleWorker.class)
                                                 .setInputData(data)
-//                                                .setConstraints(constraints)
+                                                .setConstraints(constraints)
 //                                                 .setInitialDelay(5, TimeUnit.HOURS)
                                                 .addTag(DOWNLOAD_TAG)
                                                  .build();
 
-        //Create WorkManager Instance
-        WorkManager.getInstance(this).enqueue(oneTimeDownloadRequest);
+        //Create WorkManager Instance to enqueue the request: oneTimeWorkRequest in this case
+//        WorkManager.getInstance(this).enqueue(oneTimeDownloadRequest);
+
+        //Create WorkRequest: Periodic WorkRequest in this case, repeat interval indicates the number of times the request ought to be repeated
+        PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(SampleWorker.class, 7, TimeUnit.DAYS)
+                                                                            .setInputData(data)
+                                                                            .setConstraints(constraints)
+                                                                            .setInitialDelay(5, TimeUnit.HOURS)
+                                                                            .addTag("PERIODIC")
+                                                                            .build();
+        WorkManager.getInstance(this).enqueue(periodicWorkRequest);
 
     }
 }
