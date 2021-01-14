602c377738b5824e539dd945c5ae4d24c281950a
Grant Williams <greatgrant116@gmail.com>
2021-01-14 16:01:34 +0100

    Created new activity to be redirected to when notification clicked

---
+++ app/src/main/java/com/gralliams/workmanagerplayground/AlertDetails.java
@@ -0,0 +1,14 @@
+package com.gralliams.workmanagerplayground;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlertDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_details);
+    }
+}
--- app/src/main/java/com/gralliams/workmanagerplayground/SampleWorker.java
+++ app/src/main/java/com/gralliams/workmanagerplayground/SampleWorker.java
@@ -1,8 +1,11 @@
 package com.gralliams.workmanagerplayground;
 
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -15,6 +18,7 @@ public class SampleWorker extends Worker {
     //a public static string that will be used as the key
     //for sending and receiving data
     public static final String TASK_DESC = "task_desc";
+    private static final String NOTIFICATION_ID = "notification_id";
 
     public SampleWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
         super(context, workerParams);
@@ -33,14 +37,34 @@ public class SampleWorker extends Worker {
 
     private void displayNotification(String title, String task) {
         NotificationManager notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("simplifiedcoding", "simplifiedcoding", NotificationManager.IMPORTANCE_DEFAULT);
+            CharSequence name = "Channel name";
+            String description = "Channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_ID, name, importance);
+            channel.setDescription(description);
             notificationManager.createNotificationChannel(channel);
         }
-        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), "simplifiedcoding")
+
+        // Create an explicit intent for an Activity in your app
+        Intent intent = new Intent(getApplicationContext(), AlertDetails.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, 0);
+
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(getApplicationContext(), NOTIFICATION_ID)
                 .setContentTitle(title)
                 .setContentText(task)
-                .setSmallIcon(R.mipmap.ic_launcher);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(task+" I added this extra line to test expandable notification"))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                // Set the intent that will fire when the user taps the notification
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
         notificationManager.notify(1, notification.build());
     }
 }
