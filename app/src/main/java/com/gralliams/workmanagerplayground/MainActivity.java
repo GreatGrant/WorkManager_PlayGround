303c0c7f230e22efd924f8732013394461cc9d2b
Grant Williams <greatgrant116@gmail.com>
2021-01-14 08:57:07 +0100

    Checked status of work

--- app/src/main/java/com/gralliams/workmanagerplayground/MainActivity.java
+++ app/src/main/java/com/gralliams/workmanagerplayground/MainActivity.java
@@ -1,20 +1,25 @@
 package com.gralliams.workmanagerplayground;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.work.Constraints;
 import androidx.work.Data;
 import androidx.work.NetworkType;
 import androidx.work.OneTimeWorkRequest;
 import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkInfo;
 import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import java.util.Observable;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String DOWNLOAD_TAG = "download_task";
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,22 +40,32 @@ public class MainActivity extends AppCompatActivity {
         /* Create WorkRequest: Can be one time or periodic. We'll be using one time in this case */
         OneTimeWorkRequest oneTimeDownloadRequest = new OneTimeWorkRequest.Builder(SampleWorker.class)
                                                 .setInputData(data)
-                                                .setConstraints(constraints)
+//                                                .setConstraints(constraints)
 //                                                 .setInitialDelay(5, TimeUnit.HOURS)
                                                 .addTag(DOWNLOAD_TAG)
                                                  .build();
 
         //Create WorkManager Instance to enqueue the request: oneTimeWorkRequest in this case
-//        WorkManager.getInstance(this).enqueue(oneTimeDownloadRequest);
+        WorkManager.getInstance(this).enqueue(oneTimeDownloadRequest);
 
         //Create WorkRequest: Periodic WorkRequest in this case, repeat interval indicates the number of times the request ought to be repeated
         PeriodicWorkRequest periodicWorkRequest = new PeriodicWorkRequest.Builder(SampleWorker.class, 7, TimeUnit.DAYS)
                                                                             .setInputData(data)
                                                                             .setConstraints(constraints)
-                                                                            .setInitialDelay(5, TimeUnit.HOURS)
+//                                                                            .setInitialDelay(5, TimeUnit.HOURS)
                                                                             .addTag("PERIODIC")
                                                                             .build();
-        WorkManager.getInstance(this).enqueue(periodicWorkRequest);
+//        WorkManager.getInstance(this).enqueue(periodicWorkRequest);
+
+
+        /* Check work status */
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(oneTimeDownloadRequest.getId()).observe(this, new Observer<WorkInfo>() {
+            @Override
+            public void onChanged(WorkInfo workInfo) {
+                Log.d(TAG, "onChanged: work status "+workInfo.getState());
+            }
+        });
+
 
     }
 }
